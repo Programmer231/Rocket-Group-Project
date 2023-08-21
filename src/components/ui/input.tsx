fdeb7b4d6b776c53aeccb9fa59509d9cd42a9b82
Programmer231 <sebasn327@gmail.com>
2023-08-21 13:15:30 -0400

    more fixed

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -10,7 +10,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
     return (
       <input
         type={type}
-        className="lg:flex rounded-lg border-2 border-gray-300 bg-gray-200 bg-opacity-50 p-3 text-5xl font-bold outline-offset-2 focus:border-transparent focus:border-0 font-black text-black shadow-md opacity-50 min-w-[400px]"
+        className="lg:flex rounded-lg border-2 border-gray-300 bg-gray-200 bg-opacity-50 p-3 text-5xl font-bold outline-offset-2 focus:border-transparent focus:border-0 font-black text-black shadow-md opacity-50 min-w-[200px]"
         ref={ref}
         {...props}
       />
